71c82c300d0515bfd225bc97815205b6c9290b4a
LeoChen <leo.chen@shoalter.com>
2021-10-22 15:17:21 +0800

    [add] add 13 folder

---
+++ src/13/index.ts
@@ -0,0 +1,15 @@
+/**
+ * 
+ * typeof
+ * 
+ * 
+ */
+
+const conference = {
+  name: 'MOPCON',
+  year: 2021,
+  isAddToCalendar: true,
+  website: 'https://mopcon.org/2021/',
+};
+
+type ConferenceKeys = keyof typeof conference;
